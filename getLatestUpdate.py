3dd99cddb3c636674ace09ee48280415d46f527b
Marek Veselý <marek.vesely@uhk.cz>
2025-12-01 14:26:38 +0100

    Add release notes link

--- getLatestUpdate.py
+++ getLatestUpdate.py
@@ -11,11 +11,12 @@ def walk(dev_code, version="0.0", prev_version=None, raw=None):
     res = requests.get(URL.replace("{version}", version).replace("{code}", str(dev_code)), headers={"Accept": "application/json"})
     data = res.json()
     if data["UpgradeURL"] is None:
-        return (version, raw["UpgradeURL"])
+        return (version, raw["UpgradeURL"], raw["ReleaseNoteURL"])
     next_version = re.search(r"-(\d\..*\d)", data["UpgradeURL"]).group(1)
+
     return walk(dev_code, next_version, version, data)
 
-MARKDOWN_TEMPLATE = "| {VERSION} | {MONTH} {YEAR}  | [Normal]({URL})                                                                                                                         |"
+MARKDOWN_TEMPLATE = "| {VERSION} | {MONTH} {YEAR}  | [Normal]({URL}) | [Release notes]({NOTES}) |"
 MARKDOWN_REGEX = r"5\.\d*\.\d{6}"
 
 def get_latest_known(file="README.md"):
@@ -63,14 +64,16 @@ month_dict = {
 def get_version_md() -> dict:
     out = {}
     for dev in DEVICES:
-        (version, url) = walk(dev["code"])
+        (version, url, release_notes) = walk(dev["code"])
+        
         url = url.replace(".zip", "/update.tar")
         date = re.search(r"\/(.{3})(\d{4})\/", url)
+
         month = month_dict[date.group(1)]
         year = date.group(2)
         # if (version != last_version):
             # print("NEW!")
-        md = MARKDOWN_TEMPLATE.replace("{VERSION}", version).replace("{MONTH}", month).replace("{YEAR}", year).replace("{URL}", url)
+        md = MARKDOWN_TEMPLATE.replace("{VERSION}", version).replace("{MONTH}", month).replace("{YEAR}", year).replace("{URL}", url).replace("{NOTES}", release_notes)
         out[dev["code"]] = {
             "new": version != last_version,
             "md": md,
